c7e82b23e020cbc78d12009038a5341f2ed64450
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2023-11-28 08:29:41 -0500

    Updated notes files

---
+++ 0. Notes/NotesFor1.java
@@ -0,0 +1,13 @@
+public class NotesFor1 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Hello, World!");
+            count++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
---
+++ 0. Notes/NotesOffByOne1.java
@@ -0,0 +1,11 @@
+public class NotesOffByOne1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello, World!");
+        }
+        
+        for (int i = 1; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
